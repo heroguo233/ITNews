8367dc6f05e550057c2d0ae53c438f863fcc80f9
gavin <zjw0702@gmail.com>
2019-04-10 11:42:07 +0800

    项目模板

---
+++ News/src/main/java/com/gavin/itnews/ItNewsApplication.java
@@ -0,0 +1,13 @@
+package com.gavin.itnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItNewsApplication.class, args);
+    }
+
+}
